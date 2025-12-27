b068efe2b28ec765a5460983e5b1a98e122ac1f7
young-blue <youngblue.coding@gmail.com>
2025-12-27 23:20:08 +0900

    초기 셋팅

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="min-h-screen bg-red-300 flex items-center justify-center">
+            <h1 className="text-xl font-bold text-white">TAILWIND OK1</h1>
+        </div>
+    );
+}
